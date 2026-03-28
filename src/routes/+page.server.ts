34d0b3561c2e11c07e187cf0a1d1952d21f06898
tknkaa <tknkaa@users.noreply.github.com>
2026-03-28 23:08:50 +0800

    fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
-import { users } from '$lib/db/schema';
+import { rooms } from '$lib/db/schema';
 import { db } from '$lib/db';
 
 export const load = async () => {
-	const result = await db.select().from(users);
+	const result = await db.select().from(rooms);
 	console.log(result);
 };
